c8c6191733abe07a7ec5c5c3101fcf60b0e0118c
Miel Goossens <miel.goossens@studiohyperdrive.be>
2025-02-03 17:14:16 +0100

    test html implementation

--- digital-assistant/src/app/app.component.ts
+++ digital-assistant/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { SintNiklaasComponent } from "./sint-niklaas/sint-niklaas.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, SintNiklaasComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {}
