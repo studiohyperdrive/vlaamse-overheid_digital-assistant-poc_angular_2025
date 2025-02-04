b5b1c58c64fb8ecfb4266b8e2a78b09d1a2db1b3
jonasthijs-shd <jonas.thijs@studiohyperdrive.be>
2025-02-04 12:15:37 +0100

    added routes for each view + corrected broken HTML (originated from HTML-page extracting tool) in previous commit

--- digital-assistant/src/app/app.component.ts
+++ digital-assistant/src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
-import { SintNiklaasComponent } from "./sint-niklaas/sint-niklaas.component";
-import { RenovationPremiumComponent } from './renovation-premium/renovation-premium.component';
-import { RenovationPremiumSimulationComponent } from './renovation-premium-simulation/renovation-premium-simulation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, SintNiklaasComponent, RenovationPremiumComponent, RenovationPremiumSimulationComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- digital-assistant/src/app/app.routes.ts
+++ digital-assistant/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SintNiklaasComponent } from './sint-niklaas/sint-niklaas.component';
+import { RenovationPremiumComponent } from './renovation-premium/renovation-premium.component';
+import { RenovationPremiumSimulationComponent } from './renovation-premium-simulation/renovation-premium-simulation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'sint-niklaas', component: SintNiklaasComponent },
+    { path: 'renovatie-premies', component: RenovationPremiumComponent },
+    { path: 'verbouw-premie-sim', component: RenovationPremiumSimulationComponent }
+];
