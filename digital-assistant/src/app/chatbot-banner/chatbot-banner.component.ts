94235944254e24a08a8c0ad48d5a9ee3cec99040
jonasthijs-shd <jonas.thijs@studiohyperdrive.be>
2025-02-04 14:26:17 +0100

    initial version of the banner variant of the chatbot modal

---
+++ digital-assistant/src/app/chatbot-banner/chatbot-banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatbot-banner',
+  imports: [],
+  templateUrl: './chatbot-banner.component.html',
+  styleUrl: './chatbot-banner.component.scss'
+})
+export class ChatbotBannerComponent {
+
+}
--- digital-assistant/src/app/home/home.component.ts
+++ digital-assistant/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ChatbotHeaderComponent } from '../chatbot-header/chatbot-header.component';
+import { ChatbotBannerComponent } from '../chatbot-banner/chatbot-banner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ChatbotHeaderComponent],
+  imports: [ChatbotHeaderComponent, ChatbotBannerComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
