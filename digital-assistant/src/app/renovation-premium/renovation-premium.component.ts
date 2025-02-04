5779fed01bd10932e9995999316e5863f2289a91
jonasthijs-shd <jonas.thijs@studiohyperdrive.be>
2025-02-04 10:26:37 +0100

    implemented remaining HTML pages

---
+++ digital-assistant/src/app/renovation-premium/renovation-premium.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-renovation-premium',
+  imports: [],
+  templateUrl: './renovation-premium.component.html',
+  styleUrl: './renovation-premium.component.scss'
+})
+export class RenovationPremiumComponent {
+
+}
