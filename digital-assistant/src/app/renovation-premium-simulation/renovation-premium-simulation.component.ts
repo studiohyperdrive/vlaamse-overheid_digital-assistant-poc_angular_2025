ec9a8c4a5b7954b3247af9c41d1f0dd47efa32be
Miel Goossens <miel.goossens@studiohyperdrive.be>
2025-02-05 16:54:51 +0100

    logged in state simulator

--- digital-assistant/src/app/renovation-premium-simulation/renovation-premium-simulation.component.ts
+++ digital-assistant/src/app/renovation-premium-simulation/renovation-premium-simulation.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChatbotBannerComponent } from "../chatbot-banner/chatbot-banner.component";
 import { MatIcon } from '@angular/material/icon';
 import { AutoFillAssistentMessageComponent } from '../auto-fill-assistent-message/auto-fill-assistent-message.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-renovation-premium-simulation',
@@ -9,9 +10,12 @@ import { AutoFillAssistentMessageComponent } from '../auto-fill-assistent-messag
   templateUrl: './renovation-premium-simulation.component.html',
   styleUrl: './renovation-premium-simulation.component.scss'
 })
-export class RenovationPremiumSimulationComponent {
+export class RenovationPremiumSimulationComponent implements OnInit {
   public selected: boolean = false;
   public tooltipActive: boolean = false;
+  loggedIn: boolean = false;
+
+  constructor(private route: ActivatedRoute) {}
 
   onBannerSelected(selected: boolean) {
     this.selected = selected;
@@ -19,5 +23,11 @@ export class RenovationPremiumSimulationComponent {
 
   onTooltipClick() {
     this.tooltipActive = !this.tooltipActive;
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.loggedIn = params['loggedIn'] === 'true';
+    });
   }
 }
