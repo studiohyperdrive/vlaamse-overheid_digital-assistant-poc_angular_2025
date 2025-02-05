9ce5d33cf018e78d7426505e7f8867ffed6e975c
Tom Opdebeeck <tom.opdebeeck@studiohyperdrive.be>
2025-02-05 13:12:14 +0100

    fix: set app_base_href for prod build

--- digital-assistant/src/app/app.config.ts
+++ digital-assistant/src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: APP_BASE_HREF, useValue: environment.APP_BASE_HREF },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(), provideAnimationsAsync()
---
+++ digital-assistant/src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    APP_BASE_HREF: '/vlaamse-overheid_digital-assistant-poc_angular_2025/',
+};
