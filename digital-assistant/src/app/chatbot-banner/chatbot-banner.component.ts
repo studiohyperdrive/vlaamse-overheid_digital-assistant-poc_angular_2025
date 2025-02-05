42520a218a955d0474dc6036d025622cb77f30a5
jonasthijs-shd <jonas.thijs@studiohyperdrive.be>
2025-02-05 11:59:32 +0100

    Applied requested changes on banner + navigation + some CSS changes

--- digital-assistant/src/app/chatbot-banner/chatbot-banner.component.ts
+++ digital-assistant/src/app/chatbot-banner/chatbot-banner.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-chatbot-banner',
-  imports: [MatIcon],
+  imports: [MatIcon, RouterLink],
   templateUrl: './chatbot-banner.component.html',
   styleUrl: './chatbot-banner.component.scss'
 })
